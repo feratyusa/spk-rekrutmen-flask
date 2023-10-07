da13a6ea2cf6d76e289e744d48d714476028460e
feratyusa <prabuega55@gmail.com>
2023-10-07 17:44:49 +0700

    feat: saw algorithm for number crisp

---
+++ saw/saw.py
@@ -0,0 +1,184 @@
+import pandas
+
+BENEFIT = 1
+COST = 0
+
+""" 
+crisp = {
+    "detail": ">80"
+    "weight": "40"
+}
+"""
+class Criteria():
+    # Criteria Type
+    __number = 0
+    __string = 1
+    __sub_string = 2
+    # Number Criteria Type
+    __equals_to = 0
+    __more_than_equal = 1
+    __more_than = 2
+    __less_than_equal = 3
+    __less_than = 4
+    __in_between_inclusive = 5
+    __in_between = 6
+
+    def __init__(self, name, weight, atribute, crisp_type, crisps):
+        self.name = name
+        self.weight = weight
+        self.atribute = atribute
+        self.crisp_type = crisp_type
+        self.crisps = crisps
+    
+    """ 
+    CRITERIA PROCESSS FUNCTIONS
+    """
+    # Process the Row Value Given with this Criteria
+    def get_weight_value(self, rowValue):
+        return self.check_criteria_type(rowValue)
+    
+    # Check Criteria Type
+    def check_criteria_type(self, rowValue):
+        if self.crisp_type == self.__number:
+            return self.number_criteria(rowValue)
+        elif self.crisp_type == self.__string:
+            return self.string_criteria(rowValue)
+        elif self.crisp_type == self.__sub_string:
+            return self.sub_string_criteria(rowValue)
+        else:
+            return "Error Message"
+    
+    """ 
+    NUMBER CRISP TYPE
+    """
+    def check_expression(self,value,expression,comparator):
+        return eval('x'+expression+'m',{},{'x':value,'m':comparator[0]})
+    
+    def check_in_between_inclusive(self, value, comparator):
+        return eval('c0 <= v <= c1',{}, {'c0':comparator[0], 'v':value, 'c1': comparator[1]})
+    
+    def check_in_between(self, value, comparator):
+        return eval('c0 < v < c1',{}, {'c0':comparator[0], 'v':value, 'c1': comparator[1]})
+        
+    def check_expression_value(self, value, expression, comparator):
+        if expression==self.__equals_to:
+            return self.check_expression(value, "==", comparator)
+        elif expression==self.__more_than_equal:
+            return self.check_expression(value, ">=", comparator)
+        elif expression==self.__more_than:
+            return self.check_expression(value, ">", comparator)
+        elif expression==self.__less_than_equal:
+            return self.check_expression(value, "<=", comparator)
+        elif expression==self.__less_than:
+            return self.check_expression(value, "<", comparator)
+        elif expression==self.__in_between_inclusive:
+            return self.check_in_between_inclusive(value, comparator)
+        elif expression==self.__in_between:
+            return self.check_in_between(value, comparator)
+        return "Error Message"
+            
+    def number_criteria(self, rowValue):
+        for c in self.crisps:
+            if self.check_expression_value(rowValue, c.detail[0], c.detail[1]): # If true return Weight Value
+                return c.weight
+        return "Error Message"
+    
+    """
+    STRING CRISP TYPE
+    """
+    def string_criteria(self, rowValue):
+        pass
+
+    """
+    SUB STRING CRISP TYPE
+    """
+    def sub_string_criteria(self, rowValue):
+        pass
+        
+
+class Crisp():
+    def __init__(self, detail, weight):
+        self.detail = detail
+        self.weight = weight
+    
+    def __str__(self):
+        return f"detail={self.detail}, weight={self.weight}"
+        
+
+data = pandas.read_csv('mini_dummy.csv')
+decision_matrix = []
+
+# First Criteria
+first_crisp = Crisp(detail=[2, [80]], weight=3)
+second_crisp = Crisp(detail=[5, [40,80]], weight=2)
+third_crisp = Crisp(detail=[4, [40]], weight=1)
+crisps = []
+crisps.extend([first_crisp, second_crisp, third_crisp])
+
+first_c = Criteria(
+    name="Mindset", weight=30, atribute=BENEFIT, crisp_type=0, crisps=crisps
+)
+
+# Second Criteria
+first_crisp = Crisp(detail=["Kecantikan"], weight=6)
+second_crisp = Crisp(detail=["Tata Busana"], weight=5)
+third_crisp= Crisp(detail=["Multimedia"], weight=4)
+fourth_crisp = Crisp(detail=["Tata Boga"], weight=3)
+fifth_crisp= Crisp(detail=["Teknik Listrik"], weight=2)
+sixth_crisp = Crisp(detail=["Teknik Kendaraan Ringan"], weight=1)
+crisps = []
+crisps.extend([first_crisp, second_crisp, third_crisp, fourth_crisp, fifth_crisp, sixth_crisp])
+
+second_c = Criteria(
+    name="Skillset", weight=50, atribute=BENEFIT, crisp_type=1, crisps=crisps
+)
+
+# Third Criteria
+first_crisp = Crisp(detail=["Foundation"], weight=6)
+second_crisp = Crisp(detail=["Body Painting"], weight=5)
+third_crisp= Crisp(detail=["Mascara"], weight=4)
+fourth_crisp = Crisp(detail=["Coreldraw"], weight=3)
+fifth_crisp= Crisp(detail=["Kamera"], weight=2)
+sixth_crisp = Crisp(detail=["Mesin Jahit"], weight=1)
+crisps = []
+crisps.extend([first_crisp, second_crisp, third_crisp, fourth_crisp, fifth_crisp, sixth_crisp])
+
+third_c = Criteria(
+    name="Toolset", weight=20, atribute=BENEFIT, crisp_type=2, crisps=crisps
+)
+
+# Fourth Criteria
+first_crisp = Crisp(detail=[2, [24]], weight=3)
+second_crisp = Crisp(detail=[5, [12,24]], weight=2)
+third_crisp = Crisp(detail=[4, [12]], weight=1)
+crisps = []
+crisps.extend([first_crisp, second_crisp, third_crisp])
+
+fourth_c = Criteria(
+    name="Mindset", weight=40, atribute=BENEFIT, crisp_type=0, crisps=crisps
+)
+
+criterias = []
+criterias.extend([first_c, second_c, third_c, fourth_c])
+
+# Make Decision Matrix
+# Don't include ID and Name
+data_truncate = data.drop(columns=[data.columns[0],data.columns[1]], axis=1)
+criteria_num=0
+for column in data_truncate:
+    for value in data_truncate[column].values:
+        decision_matrix.append(criterias[criteria_num].get_weight_value(value))
+    criteria_num += 1
+    break
+
+
+
+# Iterate over column names
+for column in data_truncate:
+     
+    # Select column contents by column
+    # name using [] operator
+    columnSeriesObj = data_truncate[column]
+    print(columnSeriesObj.values)
+    break
+print(decision_matrix)
